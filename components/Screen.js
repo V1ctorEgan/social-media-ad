ef95c7e6665aa6b6b8c673aae8b5c0b3eab5cec2
V1ctorEgan <victorifeanyi2004@gmail.com>
2024-06-21 19:17:30 +0100

    create folders and two screens

---
+++ components/Screen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Platform, StatusBar, View } from 'react-native';
+
+function Screen(props) {
+    return (
+   <SafeAreaView style={[styles.screen, props.style]}>
+    <View style={[styles.view, props.style]}>
+    {props.children}
+
+    </View >
+   </SafeAreaView>
+    );
+}
+const styles = StyleSheet.create({
+    screen:{
+        paddingTop: Platform.OS ==='android'? StatusBar.currentHeight : 0,
+        flex:1,
+    },
+    view:{
+        flex:1,
+    }
+})
+export default Screen;
---
+++ screens/WelcomeOneScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ImageBackground } from 'react-native';
+
+function WelcomeOneScreen(props) {
+    return (
+       <ImageBackground source={require("../assets/images/backgrounds/b")}>
+
+       </ImageBackground>
+    );
+}
+
+export default WelcomeOneScreen;
