00dfaa611b23f8a5a49275b3cc075b71229cccb7
captEze <ezetheophilus05@gmail.com>
2024-06-22 19:22:06 +0100

    comjksbjhcb

--- app/index.js
+++ app/index.js
@@ -1,20 +1,3 @@
-<<<<<<< Updated upstream
- 
-import Screen from '../components/Screen';
-import WelcomeScreen from '../screens/welcome/welcomeScreen';
-import react from 'react';
-import { Text, StyleSheet } from 'react-native';
-// import Screen from '../components/Screen';
-import Carousel from '../components/Carousel';
- 
-
-
-export default function Page() {
-    return (
-
-     <WelcomeScreen />   
-    )
-=======
 import User from '../config/user';
 
 import WelcomeScreen from "../screens/welcome/welcomeScreen";
@@ -28,5 +11,4 @@ export default function Page() {
         )
     }
     return <WelcomeScreen />;
->>>>>>> Stashed changes
 }
